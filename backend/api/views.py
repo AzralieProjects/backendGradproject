1fa57ada342db7eafde1fd8dd2efb13e42da0b40
luffyUnleashed <37274365+luffyUnleashed@users.noreply.github.com>
2020-04-20 01:46:51 +0300

    Proc

--- backend/api/views.py
+++ backend/api/views.py
@@ -9,33 +9,20 @@ from rest_framework.decorators import api_view, permission_classes, authenticati
 # import os 
 import librosa    
 
-# from .decorators import define_usage
-
-# def check_file(viewsets.ModelViewSet):
-
-
-#     @action(detail=True,method=['Post'])
-#     def convert_file(self,request,pk=none)
 
 
 @api_view(['POST'])
 # @api_view(['GET'])
 
 @permission_classes((AllowAny,))
-# import pydub
-#URL /
-# @define_usage(returns={'url_usage': 'Dict'})
-# @api_view(['GET'])
-# @permission_classes((AllowAny,)
+
 
 def api_convert(request):
     def change_file(file):
         # mp3=glob.glob('*.mp3')
         wav='fff'+'.wav'
         print('here ')
-        # pydub.AudioSegment.converter = r"C:\Users\Musa\Downloads\ffmbeg\ffmpeg-20200403-52523b6-win64-static\bin"
-        # pydub.AudioSegment.ffmpeg = r"C:\Users\Musa\Downloads\ffmbeg\ffmpeg-20200403-52523b6-win64-static\bin"
-        # pydub.AudioSegment.ffprobe =r"C:\Users\Musa\Downloads\ffmbeg\ffmpeg-20200403-52523b6-win64-static\bin"
+     
         mp3_file=file
 
         sound=pydub.AudioSegment.from_mp3(mp3_file)
@@ -45,11 +32,7 @@ def api_convert(request):
 
         print('fff ')
     # sound.export(wav,format="wav")
-        return wav  
-
-
-        
-        
+        return wav    
     def f(method):
         return "<h1>MyClub Event Calendar"+method+"</h1>"
 
@@ -63,22 +46,14 @@ def api_convert(request):
 
     #  return HttpResponse("<h1>MyClub Event Calendar</h1>")
     try:
-        # body_unicode = request.body.decode('utf-8')
-        # body = json.loads(body_unicode)
-        # content = body['content']
+      
         post_quary=request.POST
         # print(request.data)
         print(post_quary)
 
         # files=request.META
         file=request.FILES['myfile']
-        # os.system(file.mp3)
-        # file=change_file(file)
-
-        # with open('/path/I/want/to/save/file/to/file_name.pdf', 'wb') as f:
-        # f.write(r.content)
-        # os.startfile(files)
-        # os.startfile(files)
+     
 
     except:
         return HttpResponse({'error tt   '+method: 'Please provide correct file'},
@@ -87,10 +62,7 @@ def api_convert(request):
     # file = open("/path/to/your/song.mp3", "rb").read() 
 
     try:    
-        # with open(file_location, 'r') as f:
-        # file_data = file.read()
 
-        # sending response 
 
         response = HttpResponse(file, content_type='audio/mp3')
         response['Content-Disposition'] = 'attachment; filename="foo.xls"'
@@ -100,11 +72,6 @@ def api_convert(request):
         response = HttpResponse('<h1>File not exist</h1>')
 
     return response
-        # wav=os.path.splitext(mp3_file)[0]+'.wav'
-    # wav='mmm'+'.wav'
-    # sound=pydub.AudioSegment.from_mp3(myfile)
-    # sound.export(wav,format="wav") 
-    
-    
+
 
 # Create your views here.
